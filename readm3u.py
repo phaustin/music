c5f962878a2eae2f2ed73e5b7d340eb17283aad7
Phil Austin <paustin@eos.ubc.ca>
2011-02-12 19:57:34 -0800

    checkpoint

--- readm3u.py
+++ readm3u.py
@@ -1,14 +1,13 @@
 import fnmatch
 import os
-
-
+import pickle
 
 bob_matches = []
 for root, dirnames, filenames in os.walk('/home/phil/m3udir'):
     for filename in fnmatch.filter(filenames, '*.m3u'):
         bob_matches.append(os.path.join(root, filename))
 
-albums={}
+albums=[]
 for item in bob_matches:
     head,tail = os.path.split(item)
     head,tail=os.path.splitext(tail)
@@ -16,9 +15,15 @@ for item in bob_matches:
     artist,album=head.split('-')
     entry={'artist':artist,'album':album}
     with open(item) as f:
-        data = f.read()
+        data = f.read().strip()
         tracks=data.split('\n')
         entry['tracks']=tracks
+    albums.append(entry)
+
+outfile=open('albums.pic','w')
+pickle.dump(albums,outfile)
+outfile.close()
+
 
     
 ## tracknumber [u'13']
